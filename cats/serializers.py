5f40933fb022d61563f238173e03a737047d8d97
Василий Игнатьев <vas.ignatjev@yandex.ru>
2022-12-02 13:22:09 +0300

    Do lesson #10

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner, Achievement, AchievementCat
+import datetime as dt
+
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     # owner = serializers.StringRelatedField(read_only=True)
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year        
 
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:
